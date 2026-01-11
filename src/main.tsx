9f5d5b68a2fa6b06d8824bca73445b0f0d8e4aa6
Oscar Casas <oscaralexander2626@gmail.com>
2026-01-11 17:51:11 -0500

    feat: fix login style

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { GlobalErrorBoundary, initSentry } from './app'
 import { queryClient } from './shared/lib'
 import App from './app/ui/App'
 import './app/styles/index.css'
+import './app/styles/App.css';
 
 // Initialize Sentry before rendering
 initSentry()
