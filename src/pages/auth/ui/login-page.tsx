b27ba865464989daefdddce9f545ba10ad1888e1
Oscar Casas <oscaralexander2626@gmail.com>
2026-02-09 11:32:56 -0500

    feat: change login form

--- src/pages/auth/ui/login-page.tsx
+++ src/pages/auth/ui/login-page.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "@/features/auth-login";
 
 export function LoginPage() {
     return (
-        <div className="flex h-screen items-center justify-center">
+        <div className="flex h-screen items-center justify-center overflow-hidden">
             <LoginForm />
         </div>
     )
